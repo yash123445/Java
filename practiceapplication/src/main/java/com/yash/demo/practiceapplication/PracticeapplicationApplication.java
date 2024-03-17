db6fc7479f8df21949ad093a2ef47850e83d2328
Yash Pawar <92030522+yash123445@users.noreply.github.com>
2024-03-17 12:38:31 +0530

    Add files via upload

---
+++ practiceapplication/src/main/java/com/yash/demo/practiceapplication/PracticeapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.yash.demo.practiceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeapplicationApplication.class, args);
+	}
+	
+}
