6f92139acc022847a06e87a0dc34dc83e3d98887
Yash Pawar <92030522+yash123445@users.noreply.github.com>
2024-05-28 18:57:35 +0530

    Delete Spring Applications/bankApp directory

--- Spring Applications/bankApp/src/main/java/com/bankapp/bankApp/interfaces/BranchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bankapp.bankApp.interfaces;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bankapp.bankApp.entities.Branch;
-
-public interface BranchRepository extends JpaRepository<Branch,Integer>{
-
-}
