f743ff34ff9688b29d154da6751273208ef1b3b9
Yash Pawar <92030522+yash123445@users.noreply.github.com>
2024-07-09 11:14:03 +0530

    Update EmployeeRepository.java

--- Spring Applications/HumanResourceManagementSystem/HRManagementSystem/src/main/java/com/Interfaces/EmployeeRepository.java
+++ Spring Applications/HumanResourceManagementSystem/HRManagementSystem/src/main/java/com/Interfaces/EmployeeRepository.java
@@ -3,4 +3,7 @@ package com.Interfaces;
 import org.springframework.data.repository.CrudRepository;
 import com.entities.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {}
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+	Employee findById(int id);
+}
+
