5448dd0bb240e4d7c40afd63fc0939370bfc746b
Yash Pawar <92030522+yash123445@users.noreply.github.com>
2024-07-23 20:17:08 +0530

    Update Appraisal.java

--- Spring Applications/HumanResourceManagementSystem/HRManagementSystem/src/main/java/com/entities/Appraisal.java
+++ Spring Applications/HumanResourceManagementSystem/HRManagementSystem/src/main/java/com/entities/Appraisal.java
@@ -1,17 +1,125 @@
 package com.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
-@Table(name="appraisal")
+@Table(name = "Appraisal")
 public class Appraisal {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private int apr_id;
-	
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "emp_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "self_rating")
+    private String selfRating;
+    
+    @Column(name="self_remark")
+    private String selfRemark;
+
+    @Column(name = "reviewer_rating")
+    private String reviewerRating;
+
+    @Column(name = "reviewer_remarks")
+    private String reviewerRemarks;
+
+    @Column(name = "is_submitted")
+    private boolean isSubmitted;
+
+    @Column(name = "appraisal_date")
+    private Date appraisalDate;
+
+    public Appraisal() {}
+
+    
+
+    public Appraisal(Long id, Employee employee, String selfRating, String selfRemark, String reviewerRating,
+			String reviewerRemarks, boolean isSubmitted, Date appraisalDate) {
+		super();
+		this.id = id;
+		this.employee = employee;
+		this.selfRating = selfRating;
+		this.selfRemark = selfRemark;
+		this.reviewerRating = reviewerRating;
+		this.reviewerRemarks = reviewerRemarks;
+		this.isSubmitted = isSubmitted;
+		this.appraisalDate = appraisalDate;
+	}
+
+
+
+	// Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public String getSelfRating() {
+        return selfRating;
+    }
+
+    public void setSelfRating(String selfRating) {
+        this.selfRating = selfRating;
+    }
+
+    public String getReviewerRating() {
+        return reviewerRating;
+    }
+
+    public void setReviewerRating(String reviewerRating) {
+        this.reviewerRating = reviewerRating;
+    }
+
+    public String getReviewerRemarks() {
+        return reviewerRemarks;
+    }
+
+    public void setReviewerRemarks(String reviewerRemarks) {
+        this.reviewerRemarks = reviewerRemarks;
+    }
+
+    public boolean isSubmitted() {
+        return isSubmitted;
+    }
+
+    public void setSubmitted(boolean isSubmitted) {
+        this.isSubmitted = isSubmitted;
+    }
+
+    public Date getAppraisalDate() {
+        return appraisalDate;
+    }
+
+    public void setAppraisalDate(Date appraisalDate) {
+        this.appraisalDate = appraisalDate;
+    }
+
+
+
+	public String getSelfRemark() {
+		return selfRemark;
+	}
+
+
+
+	public void setSelfRemark(String selfRemark) {
+		this.selfRemark = selfRemark;
+	}
+    
 }
+
